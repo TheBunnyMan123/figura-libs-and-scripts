12fd9938a37f08f3569407d67917e3c4ada131c1
TheBunnyMan123 <teguemcnair@gmail.com>
2024-04-28 21:03:23 -0500

    Add files via upload

--- BunnyChatUtils.lua
+++ BunnyChatUtils.lua
@@ -387,8 +387,13 @@ BunnyChatUtils:register(function(_, chatJson, rawText)
             pcall(function()
                 local plrName = chatJson.with[1]
                 local plr = ""
-                for _, v in ipairs(plrName.extra) do
-                    plr = plr .. v
+                
+                if plrName.extra then
+                    for _, v in ipairs(plrName.extra) do
+                        plr = plr .. v
+                    end
+                else
+                    plr = plrName.insertion
                 end
 
                 local msg = chatJson.with[2]
@@ -439,8 +444,13 @@ BunnyChatUtils:register(function(_, chatJson, rawText)
             pcall(function()
                 local plrName = chatJson.with[1]
                 local plr = ""
-                for _, v in ipairs(plrName.extra) do
-                    plr = plr .. v
+                
+                if plrName.extra then
+                    for _, v in ipairs(plrName.extra) do
+                        plr = plr .. v
+                    end
+                else
+                    plr = plrName.insertion
                 end
 
                 local msg = chatJson.with[2]
@@ -491,8 +501,13 @@ BunnyChatUtils:register(function(_, chatJson, rawText)
             pcall(function()
                 local plrName = chatJson.with[1]
                 local plr = ""
-                for _, v in ipairs(plrName.extra) do
-                    plr = plr .. v
+                
+                if plrName.extra then
+                    for _, v in ipairs(plrName.extra) do
+                        plr = plr .. v
+                    end
+                else
+                    plr = plrName.insertion
                 end
 
                 local task = chatJson.with[2].with[1]
@@ -530,8 +545,13 @@ BunnyChatUtils:register(function(_, chatJson, rawText)
         if chatJson.translate == "chat.type.advancement.goal" then
             local plrName = chatJson.with[1]
             local plr = ""
-            for _, v in ipairs(plrName.extra) do
-                plr = plr .. v
+            
+            if plrName.extra then
+                for _, v in ipairs(plrName.extra) do
+                    plr = plr .. v
+                end
+            else
+                plr = plrName.insertion
             end
 
             local task = chatJson.with[2].with[1]
@@ -568,8 +588,13 @@ BunnyChatUtils:register(function(_, chatJson, rawText)
         if chatJson.translate == "chat.type.advancement.challenge" then
             local plrName = chatJson.with[1]
             local plr = ""
-            for _, v in ipairs(plrName.extra) do
-                plr = plr .. v
+
+            if plrName.extra then
+                for _, v in ipairs(plrName.extra) do
+                    plr = plr .. v
+                end
+            else
+                plr = plrName.insertion
             end
 
             local task = chatJson.with[2].with[1]
